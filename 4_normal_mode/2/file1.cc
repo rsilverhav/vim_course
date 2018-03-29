8ebc065309c143caec1c6ecb2cf289f72d4d706d
Robin Silverhav <robin.silverhav@cybercom.com>
2018-03-29 15:51:25 +0200

    added movement 2

---
+++ 4_normal_mode/2/file1.cc
@@ -0,0 +1,10 @@
+void randomFunction() {
+  std::string str = "this is an important string";
+
+  if (str == "always false") {
+    // This is a comment that says nothing about the code, lets keep it
+    const int i = 5 % CONST_INT;
+    someVector.push_back(i);
+    print(str);
+  }
+}
---
+++ 4_normal_mode/2/start.sh
@@ -0,0 +1,1 @@
+vim -c "call cursor(2, 14)" file1.cc
