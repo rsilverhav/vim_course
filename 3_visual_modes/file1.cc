4c898000ec9e8b764bf4324aab8cdb79dd3abdc8
Robin Silverhav <robin.silverhav@cybercom.com>
2018-03-29 16:10:07 +0200

    3 is now a vim test

--- 3_visual_modes/file1.cc
+++ 3_visual_modes/file1.cc
@@ -2,5 +2,10 @@
 
 const std::string getError() {
   std::cout << "REMOVE this is debug" << std::endl;
+  arr[0] = "first";
+  arr[1] = "second";
+  arr[2] = "another";
+  arr[3] = "NaN";
+  arr[4] = "Arrays starts at 1";
   return errors.at(0);
 }
